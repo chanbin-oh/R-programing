12d181183e7935cdc4df4dc6320414ab7ef83a77
chanbin-oh <48703275+chanbin-oh@users.noreply.github.com>
2019-05-26 18:25:04 +0900

    Add files via upload

--- text_mining.R
+++ text_mining.R
@@ -19,6 +19,10 @@ useNIADic()
 txt <- readLines("hiphop.txt")
 head(txt)
 
+[1] "\"보고 싶다"                  "이렇게 말하니까 더 보고 싶다" "너희 사진을 보고 있어도"      "보고 싶다"                   
+[5] "너무 야속한 시간"             "나는 우리가 밉다" 
+
+
 ### 4. 특수문자 제거하기
 
 install.packages("stringr")
@@ -34,6 +38,8 @@ txt <- str_replace_all(txt, "\\W", " ")
 ### 1. 명사 추출하기
 
 extractNoun("대한민국의 영토는 한반도와 그 부속도서로 한다")
+
+[1] "대한"     "민국"     "영토"     "한반도와" "부속도서" "한"
 
 ### 2.
 
@@ -60,3 +66,32 @@ df_word <- filter(df_word, nchar(word) >= 2)
 #############################################################################################################
 
 #### 워드 클라우드 만들기
+
+### 1. 패키지 준비하기
+# 패키지 설치
+install.packages("wordcloud")
+
+# 패키지 로드
+library(wordcloud)
+library(RColorBrewer)
+
+
+### 2. 단어 색상 목록 만들기
+
+# Dark2 색상 목록에서 8개 색상 추출
+pal <- brewer.pal(8, "Dark2")
+
+### 3. 난수 고정하기
+
+set.seed(1234)
+
+### 4. 워드 클라우드 만들기
+
+wordcloud(words = df_word$word,
+          freq = df_word$freq,
+          min.freq = 2,
+          max.words = 200,
+          random.order = F,
+          rot.per = .1,
+          scale = c(4, 0.3),
+          colors = pal)
